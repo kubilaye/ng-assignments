903206d41510b850179ffb0934d647c6f8ce1050
Kubilay Erdogan <kublaios@gmail.com>
2018-03-20 15:21:03 +0300

    creating a simple observable

--- observables/src/app/user/user.component.ts
+++ observables/src/app/user/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = params['id'];
+        this.id = +params['id'];
       }
     );
   }
