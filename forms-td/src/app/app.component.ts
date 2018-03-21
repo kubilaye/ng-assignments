00e2643e9d43fbaedca55ae718819565977c11ab
Kubilay Erdogan <kublaios@gmail.com>
2018-03-21 09:02:52 +0300

    alternative submitting using ViewChild()

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,11 +7,15 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('myForm') signupForm: NgForm; // alternative
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   submitForm(form: NgForm) {
     console.log(form);
+    // using view child (alternative)
+    console.log(this.signupForm);
   }
 }
