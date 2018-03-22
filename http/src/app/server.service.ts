5d1dc8ecea88e50c7727655f98e535c298d8924e
Kubilay Erdogan <kublaios@gmail.com>
2018-03-22 16:58:24 +0300

    catching the errors

--- http/src/app/server.service.ts
+++ http/src/app/server.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Headers, Http} from '@angular/http';
 import 'rxjs/Rx';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ServerService {
@@ -11,7 +12,16 @@ export class ServerService {
     const headers = new Headers({
       'Content-Type': 'application/json',
     });
-    // return this.http.post(
+    return this.http.post(
+      'https://angular-playground-1.firebaseio.com/data',
+      servers,
+      {headers: headers}
+    ).catch(
+      (error: Response) => {
+        // return Observable.throw(error);
+        return Observable.throw('something went wrong');
+      }
+    );
     return this.http.put( // override the existing data
       'https://angular-playground-1.firebaseio.com/data.json',
       servers,
